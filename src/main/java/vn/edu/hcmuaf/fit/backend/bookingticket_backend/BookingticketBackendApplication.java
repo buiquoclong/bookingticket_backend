5522f0789793941b3555e275b6bcb06a1514d36e
Bùi Quốc Long <20130314@st.hcmuaf.edu.vn>
2024-04-13 21:05:42 +0700

    create

---
+++ src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/BookingticketBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.backend.bookingticket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingticketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingticketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/BookingticketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.backend.bookingticket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingticketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
