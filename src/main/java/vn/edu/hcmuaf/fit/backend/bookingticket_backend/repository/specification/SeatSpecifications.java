16548fc41ff75839815de39013b1b998fa15fafd
buiquoclong <buiquoclongkt123@gmail.com>
2026-02-11 11:01:07 +0700

    Update logic and logic function in Route, Seat and Vehicle API

--- src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/repository/specification/SeatSpecifications.java
+++ src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/repository/specification/SeatSpecifications.java
@@ -1,28 +1,27 @@
 package vn.edu.hcmuaf.fit.backend.bookingticket_backend.repository.specification;
 
 import org.springframework.data.jpa.domain.Specification;
-import vn.edu.hcmuaf.fit.backend.bookingticket_backend.model.Review;
 import vn.edu.hcmuaf.fit.backend.bookingticket_backend.model.Seat;
-import vn.edu.hcmuaf.fit.backend.bookingticket_backend.model.User;
 
 public class SeatSpecifications {
+
     public static Specification<Seat> hasId(Integer id) {
         return (root, query, criteriaBuilder) ->
                 id == null ? null : criteriaBuilder.equal(root.get("id"), id);
     }
 
-
     public static Specification<Seat> hasName(String name) {
         return (root, query, criteriaBuilder) ->
                 name == null ? null : criteriaBuilder.like(root.get("name"), "%" + name + "%");
     }
-    public static Specification<Seat> hasUserKindVehicleName(String kindVehiclename) {
+
+    public static Specification<Seat> hasStatus(Integer status) {
         return (root, query, criteriaBuilder) ->
-                kindVehiclename == null ? null : criteriaBuilder.like(root.get("kindVehicle").get("name"), "%" + kindVehiclename + "%");
+                status == null ? null : criteriaBuilder.equal(root.get("status"), status);
     }
 
-    public static Specification<Seat> hasRating(Integer rating) {
+    public static Specification<Seat> hasKindVehicleId(Integer kindVehicleId) {
         return (root, query, criteriaBuilder) ->
-                rating == null ? null : criteriaBuilder.equal(root.get("rating"), rating);
+                kindVehicleId == null ? null : criteriaBuilder.equal(root.get("kindVehicle").get("id"), kindVehicleId);
     }
 }
--- src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/repository/specification/VehicleSpecifications.java
+++ src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/repository/specification/VehicleSpecifications.java
@@ -1,7 +1,6 @@
 package vn.edu.hcmuaf.fit.backend.bookingticket_backend.repository.specification;
 
 import org.springframework.data.jpa.domain.Specification;
-import vn.edu.hcmuaf.fit.backend.bookingticket_backend.model.Seat;
 import vn.edu.hcmuaf.fit.backend.bookingticket_backend.model.Vehicle;
 
 public class VehicleSpecifications {
@@ -10,18 +9,39 @@ public class VehicleSpecifications {
                 id == null ? null : criteriaBuilder.equal(root.get("id"), id);
     }
 
-
     public static Specification<Vehicle> hasName(String name) {
         return (root, query, criteriaBuilder) ->
-                name == null ? null : criteriaBuilder.like(root.get("name"), "%" + name + "%");
-    }
-    public static Specification<Vehicle> hasUserKindVehicleName(String kindVehiclename) {
-        return (root, query, criteriaBuilder) ->
-                kindVehiclename == null ? null : criteriaBuilder.like(root.get("kindVehicle").get("name"), "%" + kindVehiclename + "%");
+                (name == null || name.isEmpty()) ? null :
+                        criteriaBuilder.like(root.get("name"), "%" + name + "%");
     }
 
-    public static Specification<Vehicle> hasvehicleNumber(String vehicleNumber) {
+    public static Specification<Vehicle> hasKindVehicleId(Integer kindVehicleId) {
         return (root, query, criteriaBuilder) ->
-                vehicleNumber == null ? null : criteriaBuilder.like(root.get("vehicleNumber"), "%" + vehicleNumber + "%");
+                kindVehicleId == null ? null :
+                        criteriaBuilder.equal(root.get("kindVehicle").get("id"), kindVehicleId);
+    }
+
+    public static Specification<Vehicle> hasVehicleNumber(String vehicleNumber) {
+        return (root, query, criteriaBuilder) ->
+                (vehicleNumber == null || vehicleNumber.isEmpty()) ? null :
+                        criteriaBuilder.like(root.get("vehicleNumber"), "%" + vehicleNumber + "%");
+    }
+
+    public static Specification<Vehicle> hasValue(Integer value) {
+        return (root, query, criteriaBuilder) ->
+                value == null ? null :
+                        criteriaBuilder.equal(root.get("value"), value);
+    }
+
+    public static Specification<Vehicle> hasStatus(Integer status) {
+        return (root, query, criteriaBuilder) ->
+                status == null ? null :
+                        criteriaBuilder.equal(root.get("status"), status);
+    }
+
+    public static Specification<Vehicle> hasUserKindVehicleName(String kindVehiclename) {
+        return (root, query, criteriaBuilder) ->
+                (kindVehiclename == null || kindVehiclename.isEmpty()) ? null :
+                        criteriaBuilder.like(root.get("kindVehicle").get("name"), "%" + kindVehiclename + "%");
     }
 }
