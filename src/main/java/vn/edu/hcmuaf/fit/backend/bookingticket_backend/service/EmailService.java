586fc89353444f9657bacabc4f68741b0ec3f925
buiquoclong <buiquoclongkt123@gmail.com>
2026-02-12 09:36:32 +0700

    Update logic and fix logic function in EmailService, PromotionService and RouteSevice

--- src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/service/EmailService.java
+++ src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/service/EmailService.java
@@ -6,4 +6,5 @@ public interface EmailService {
     void sendVerificationEmail(String to, String code) throws MessagingException;
     void sendForgotPasswordEmail(String email, String newPassword) throws MessagingException;
     void sendBookingDetailsEmail(int bookingId) throws MessagingException;
+    void sendAccountCreatedByAdminEmail(String to, String name, String password) throws MessagingException;
 }
