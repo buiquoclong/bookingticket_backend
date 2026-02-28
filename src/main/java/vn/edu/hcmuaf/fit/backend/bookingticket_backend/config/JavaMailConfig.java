47971600b280328b349b34a74956b21674df53fb
buiquoclong <buiquoclongkt123@gmail.com>
2026-02-28 09:07:21 +0700

    Update logic and fix comment in JavaMailConfig

--- src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/config/JavaMailConfig.java
+++ src/main/java/vn/edu/hcmuaf/fit/backend/bookingticket_backend/config/JavaMailConfig.java
@@ -12,24 +12,6 @@ import java.util.Properties;
 @Configuration
 public class JavaMailConfig {
 
-//    public JavaMailSender getJavaMailSeder(){
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost("smtp.gmail.com");
-//        mailSender.setPort(587);
-//
-//        mailSender.setUsername("roadline.booking@gmail.com");
-//        mailSender.setPassword("roadline12345678");
-//        mailSender.setDefaultEncoding("UTF-8");
-//
-//        Properties props = mailSender.getJavaMailProperties();
-//        props.put("mail.transport.protocol", "smtp");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.debug", "true");
-//
-//        return mailSender;
-//
-//    }
     @Value("${mailServer.host}")
     private String host;
 
